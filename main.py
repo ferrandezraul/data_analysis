7190d612406d67e4a89453d76b31a7731e0d3252
Raul Ferrandez <raul.ferrandez@idiada.com>
2019-05-02 16:41:59 +0200

    Use the cars data

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import pandas as pd
 
-df = pd.read_csv("data_sets/wine_quality/winequality-white.csv", sep=';')
+headers = ["symboling", "normalized-losses", "make", "fuel-type", "aspiration", "num-of-doors", "body-style",
+           "drive-wheels", "engine-location", "wheel-base", "length", "width", "height", "curb-weight", "engine-type",
+           "num-of-cylinders", "engine-size", "fuel-system", "bore", "stroke", "compression-ratio", "horsepower",
+           "peak-rpm", "city-mpg", "highway-mpg", "price"]
+
+df = pd.read_csv("data_sets/cars/auto.csv", sep=',', names=headers)
 
 print("Printing data head")
 print(df.head(5))
@@ -22,6 +27,6 @@ print("Printing data info")
 print(df.info())
 
 print("\n")
-print("Printing column 'citric acid' of the data frame")
-print(df["citric acid"])
+print("Printing column 'make' of the data frame")
+print(df["make"])
 
